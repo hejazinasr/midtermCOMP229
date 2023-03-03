b8a23c4cb7f351db9ebfbedf2167a5c52b7d1482
hejazinasr <123108083+hejazinasr@users.noreply.github.com>
2023-03-03 15:02:02 -0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI":"mongodb+srv://mohamadhejazi:O56EmNjd0EcvYXY8@clusterassignment2.polewsy.mongodb.net/contact?retryWrites=true&w=majority"
+  "URI":"mongodb+srv://mohamadhejazi:O56EmNjd0EcvYXY8@clusterassignment2.polewsy.mongodb.net/AsaNetoComp229?retryWrites=true&w=majority"
 };
